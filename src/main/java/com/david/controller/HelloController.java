64565a59bdc98f739a5d9d42c0b929a4682735f6
davidchen <davidchen@bluetechnology.com.tw>
2024-01-24 20:56:52 +0800

    modify

--- src/main/java/com/david/controller/HelloController.java
+++ src/main/java/com/david/controller/HelloController.java
@@ -1,2 +1,13 @@
-package com.david.controller;public class HelloController {
+package com.david.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public String hello() {
+        return "Hello";
+    }
 }
