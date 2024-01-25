ffd399d1915867d633cbc879fa34d27b239e5a42
davidchen <davidchen@bluetechnology.com.tw>
2024-01-25 11:59:35 +0800

    modify package to war file

--- src/main/java/com/david/JenkinsTestApplication.java
+++ src/main/java/com/david/JenkinsTestApplication.java
@@ -2,9 +2,15 @@ package com.david;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsTestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
